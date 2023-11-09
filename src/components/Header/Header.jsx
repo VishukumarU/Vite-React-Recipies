099dc042cc63011ea9d9813be903a0e674f1494c
Vishu Kumar <1992vishukumar@gmail.com>
2023-11-09 19:13:20 +0530

    header layout and buttons added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Toolbar, Tooltip, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import FoodBankIcon from '@mui/icons-material/FoodBank';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
@@ -10,13 +12,22 @@ const Header = () => {
                 <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
                     <MenuIcon />
                 </IconButton>
-                <Typography sx={{ flexGrow: 1 }} />
-                <Button component={RouterLink} to="/login" color="inherit" style={{
-                    '&:hover': {
-                        backgroundColor: '#fff',
-                        color: '#3c52b2',
-                    }
-                }}>Login</Button>
+                <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'flex-end' }}>
+                    <FoodBankIcon sx={{ fontSize: "2.5rem" }} />
+                    <Typography sx={{ ml: "1rem", fontSize: "1.3rem" }}>React Recipes</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Tooltip title="Settings" >
+                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                            <SettingsIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Button component={RouterLink} to="../login" color="inherit" sx={{
+                        '&:hover': {
+                            color: 'inherit'
+                        }
+                    }}>Login</Button>
+                </Box>
             </Toolbar>
         </AppBar>
     );
