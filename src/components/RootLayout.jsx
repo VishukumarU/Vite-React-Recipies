cdcfabfcbd43982ddfe70460abd7bc2954341e90
Vishu Kumar <1992vishukumar@gmail.com>
2024-01-19 09:46:07 +0530

    added tailwind config and updated the styles

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
-
+import NavBar from "./NavBar/NavBar";
+import styles from "../utils/styles";
 
 const RootLayout = () => {
-    return (
-      <>
-        <Header></Header>
-        <Outlet />
-      </>
-    );
-}
+  return (
+    <div className={styles.boxWidth}>
+      <NavBar></NavBar>
+      <Outlet />
+    </div>
+  );
+};
 
 export default RootLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#10445e"
-      }
+        primary: "#00040f",
+        secondary: "#EBB559",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-
   },
   plugins: [],
-}
+};
 
